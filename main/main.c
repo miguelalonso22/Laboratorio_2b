cf85e3fc9f20eceb4f742b1659f73fb75c9e75cb
miguelalonso22 <miguelalonsotormes@gmail.com>
2024-05-21 12:08:00 -0300

    wifi-funcionando-primera-vez

--- main/main.c
+++ main/main.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
 // #include <string.h>
 // #include "esp_wifi.h"
 // #include "esp_event.h"
-#include "esp_netif.h"
 // #include "esp_system.h"
+#include <stdio.h>
+#include "esp_netif.h"
 
 #include <string.h>
 #include "freertos/FreeRTOS.h"
@@ -22,40 +22,40 @@
 #define EXAMPLE_ESP_WIFI_CHANNEL   CONFIG_ESP_WIFI_CHANNEL
 #define EXAMPLE_MAX_STA_CONN       CONFIG_ESP_MAX_STA_CONN
 
-static const char *TAG = "wifi softAP";
+// static const char *TAG = "wifi softAP";
 
 static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
 {
     if (event_id == WIFI_EVENT_AP_STACONNECTED) {
         wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
-        // ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",
-        //          MAC2STR(event->mac), event->aid);
     } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
         wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
-        // ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d",
-        //          MAC2STR(event->mac), event->aid);
     }
 }
 
 void app_main(void)
 {
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
 // Inicializar la interfaz de red
     esp_netif_init();
 // Crear el loop de eventos por defecto
     esp_event_loop_create_default();
-
+// Crear WiFi AP por defecto
     esp_netif_create_default_wifi_ap();
-  
-    
+// Registrar el manejador de eventos de WiFi 
     esp_event_handler_instance_register(WIFI_EVENT,
                                         ESP_EVENT_ANY_ID,
                                         &wifi_event_handler,
                                         NULL,
                                         NULL);
-
+// Inicializar el stack de WiFi
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     esp_wifi_init(&cfg);
-
+    
 // Configurar las opciones del AP
     wifi_config_t ap_config = {
         .ap = {
@@ -72,5 +72,6 @@ void app_main(void)
     esp_wifi_set_mode(WIFI_MODE_AP);
     esp_wifi_set_config(WIFI_IF_AP, &ap_config);
     esp_wifi_start();
+    printf("WiFi started\n");
 
 }
